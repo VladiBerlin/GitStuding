703a907a1899b7c513dc5475569b758746f3353b
Vladislav Dovnar <dovnarvladislav@gmail.com>
2021-11-28 02:03:43 +0100

    ready calculator

--- calculatorWhithJsAndCss/script.js
+++ calculatorWhithJsAndCss/script.js
@@ -4,8 +4,9 @@ window.onload =() => {
     let a = '';
     let b = '';
     let sign = '';
+    let finish = false;
         
-    const nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '0'];
+    const nums = ['.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
     const signs = ['+', '-', 'X', '/'];
   
     for(var i = 0; i < btn.length; i++){
@@ -16,22 +17,57 @@ window.onload =() => {
     function funCl(event){
         let key = event.target.innerHTML;
 
-        if(event.target.innerHTML == 'ac'){
+        if(!event.target.classList.contains('btn')){
+            return;
+        }
+
+        else if(event.target.innerHTML == 'ac'){
             console.log('ac');
             par.textContent = '0';
             a = '';
             b = '';
-        }
-               
-        else if(!event.target.classList.contains('btn')){
-            return;
+            finish = false;
         }
 
-        else if(par.innerHTML == '0' && event.target.innerHTML != 'ac'){
+        else if(nums.includes(key) && finish == true){
+            
+            par.innerHTML = key;
+            a = par.innerHTML;
+            b = '';
+            finish = false;
+        }
+
+        else if(event.target.innerHTML == '.' && a == '' || event.target.innerHTML == '.' && a == '0'){
+            a = '0.';
+            par.innerHTML = a;
+        }
+
+        else if(event.target.innerHTML == '.' && sign != '' && b == '' || event.target.innerHTML == '.' && sign != '' && b == '0'){
+            b = '0.';
+            par.innerHTML = b;
+        }
+
+        else if(event.target.innerHTML == '√' && sign == '' && b == ''){
+            a = Math.sqrt(a);
+            par.innerHTML = a;
+        }
+
+        else if(event.target.innerHTML == '√' && b != ''){
+            b = Math.sqrt(b);
+            par.innerHTML = b;
+        }
+
+        else if(signs.includes(key) && finish == true){
+            finish = false;
+            sign = key;
+            par.innerHTML = key; 
+        }
+
+        else if(par.innerHTML == '0' && event.target.innerHTML != 'ac' && event.target.innerHTML != '+/-'){
             a = key;
             par.innerHTML = a;
         }
-        
+       
         else if(nums.includes(key) && sign == ''){
             a += key;
             par.innerHTML = a;
@@ -40,6 +76,7 @@ window.onload =() => {
 
         else if(signs.includes(key) && b == ''){
             sign = key;
+            par.innerHTML = sign;
             console.log('sign = ' + key);
         }
 
@@ -50,35 +87,37 @@ window.onload =() => {
         }
 
         else if(signs.includes(key) &&  b!=''){
-           
+            finish = false;
             switch(sign){
-               case '+': a = Number(a) + Number(b); b = ''; sign = key;
+               case '+': a = Number(a) + Number(b); b = ''; sign = key; par.innerHTML = sign;
                break;
-               case '-': a = Number(a) - Number(b); b = ''; sign = key;
+               case '-': a = Number(a) - Number(b); b = ''; sign = key; par.innerHTML = sign;
                break;
-               case 'X': a = Number(a) * Number(b); b = ''; sign = key;
+               case 'X': a = Number(a) * Number(b); b = ''; sign = key; par.innerHTML = sign;
                break;
-               case '/': a = Number(a) / Number(b); b = ''; sign = key;
+               case '/': a = Number(a) / Number(b); b = ''; sign = key; par.innerHTML = sign;
                break;
            }
-           console.log('b = ' + b + 'a = ' + a);
+                     console.log('b = ' + b + 'a = ' + a);
         }
 
         else if(event.target.innerHTML === '='){
             switch(sign){
                 case '+': a = Number(a) + Number(b); b = ''; par.textContent = a; sign = ''; 
-                b = '';
+                b = ''; finish = true;
                 break;
                 case '-': a = Number(a) - Number(b); b = ''; par.textContent = a; sign = '';
-                b = '';
+                b = ''; finish = true;
                 break;
                 case 'X': a = Number(a) * Number(b); b = ''; par.textContent = a; sign = '';
-                b = '';
+                b = ''; finish = true;
                 break;
                 case '/': a = Number(a) / Number(b); b = ''; par.textContent = a; sign = '';
-                b = '';
+                b = ''; finish = true;
                 break;
             }
+
+            console.log(finish);
         }
 
         else if(event.target.innerHTML === '+/-' && par.innerHTML == '0'){
@@ -103,7 +142,7 @@ window.onload =() => {
         else if(event.target.innerHTML === '+/-' && Number(par.innerHTML) < 0 && b == ''){
             par.innerHTML = Number(par.innerHTML) * (-1);
             a = par.innerHTML;
-        }
+        }        
     }
 }
 
